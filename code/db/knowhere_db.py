8d3dcef0fe8cfd41e580b6e24fe48b88440b3773
Bill Best <billtholomew@gmail.com>
2017-03-18 16:48:28 -0400

    Uplload to s3 examples

--- code/db/knowhere_db.py
+++ code/db/knowhere_db.py
@@ -2,6 +2,8 @@ import warnings
 import pymongo
 import ConfigParser
 import pandas as pd
+from numpy import mean
+#from numpy.ndarray import astype
 from bson.objectid import ObjectId
 from dateutil.parser import parse as parse_date
 
@@ -23,15 +25,22 @@ class Reader:
                                                                         ip=self.ip, db_name=self.db_name)
         self.client = pymongo.MongoClient(connect_string)
         self.db = self.client[self.db_name]   
+
+    def get_user_id(self, username):
+        user_id = None
+        user_data = self.read('users', filter_args={'username': username}, find_one=True)
+        if user_data:
+            user_id = user_data.get('_id')
+        if user_id is None:
+            raise Exception('Could not get user_id for user {}'.format(username))
+        return user_id
         
     def build_filter(self, username=None, user_id=None, sensor=None, min_date=None, max_date=None, include_max_date=False): 
         filter_args = {}
         if username:
-            user_data = self.filter_collection('users', filter_args={'username': username}, find_one=True)
-            if user_data:
-                user_id = user_data.get('_id')
+            self.get_user_id(username)
         if user_id:
-            filter_args['user_id'] = user_id            
+            filter_args['user_id'] = ObjectId(user_id)
         if sensor:
             filter_args['sensor'] = sensor
         if min_date or max_date:
@@ -45,41 +54,51 @@ class Reader:
                     date_filter['$lt'] = parse_date(max_date)
             filter_args['timestamp'] = date_filter
         return filter_args
-        
-    def filter_collection(self, collection, filter_args={}, find_one=False):
-        if find_one:
-            return self.db[collection].find_one(filter_args)
-        return self.db[collection].find(filter_args)
 
     def get_dataframe(self, collection, filter_args={}):
-        data = [entry for entry in self.filter_collection(collection, filter_args)]
+        data = [entry for entry in self.read(collection, filter_args)]
         return pd.DataFrame(data)
         
     def get_dataframe_unrolled(self, collection, username=None, user_id=None, sensor=None, min_date=None, max_date=None, include_max_date=False):
+        def entry_to_rows(entry):
+            return map(lambda (data_name, data_raw):
+                    {'timestamp': entry['timestamp'], 'user_id': entry['user_id'], 'sensor': entry['sensor'], 
+                    'data_name': data_name, 'data_raw': data_raw},
+                    entry['data'].iteritems()
+                    )
+                    
         # build filter_args
         filter_args = self.build_filter(username, user_id, sensor, min_date, max_date, include_max_date)
-        data = []
-        for entry in self.filter_collection(collection, filter_args):
-            for data_name, data_raw in entry['data'].iteritems():
-                row = {'timestamp': entry['timestamp'], 'user_id': entry['user_id'], 'sensor': entry['sensor']}
-                row['data_name'] = data_name
-                row['data_raw'] = data_raw
-                data.append(row)
+        entries = self.read(collection, filter_args)
+        # turn each entry into a list (called row), so we have a list of list
+        rows = map(lambda entry: entry_to_rows(entry), entries)
+        # join all the rows into one list
+        data = reduce(lambda a,b: a + b, rows, [])
         return pd.DataFrame(data)
         
     def get_dataframe_pivoted(self, collection, username=None, user_id=None, sensor=None, min_date=None, max_date=None, include_max_date=False):
+        def agg_data(d):
+            try:
+                return mean(float(d))
+            except:
+                return list(d)
+            return None
         # build filter_args
         filter_args = self.build_filter()
         if not user_id and not username:
             warnings.warn('Excluding user_id from filter can cause errors during pivot', Warning)
         # get the unrolled version so we can pivot the dataframe
         rdf = self.get_dataframe_unrolled(collection, username, user_id, sensor, min_date, max_date, include_max_date)
-        #rdf.timestamp = pd.to_datetime(rdf.timestamp)
         rdf['sensor_name'] = rdf.apply(lambda row: row.sensor + ' (' + row.data_name + ')', axis=1)
-        rdf.drop(['sensor', 'data_name'], axis=1, inplace=True)
-        rdf_pivoted = rdf.pivot(index='timestamp', columns='sensor_name', values='data_raw')
+        #rdf.drop(['sensor', 'data_name'], axis=1, inplace=True)
+        rdf_pivoted = rdf.pivot_table(index='timestamp', columns='sensor_name', values='data_raw', aggfunc=agg_data)
         return rdf_pivoted
-            
+
+    def read(self, collection, filter_args={}, find_one=False):
+        if find_one:
+            return self.db[collection].find_one(filter_args)
+        return self.db[collection].find(filter_args)
+        
     def close(self):
         self.client.close()
 
@@ -98,5 +117,15 @@ class Writer(Reader):
         self.connect()
 
     def write_dataframe_to_collection(self, df, collection):
-        data_to_insert = df.to_dict(orient='records')
-        self.db[collection].insert_many(data_to_insert)
+        data = df.to_dict(orient='records')
+        return self.write(collection=collection, data=data, insert_many=True)
+        
+    def write(self, collection, data, insert_one=False, update_filter={}, upsert=True):
+        if insert_one:
+            return self.db[collection].insert_one(data)
+        return self.db[collection].insert_many(data)
+        
+    def overwrite(self, collection, data, insert_one=False, update_filter={}, upsert=True):
+        if insert_one:
+            return self.db[collection].update_one(update_filter, {'$set': data}, upsert)
+        return self.db[collection].update_many(update_filter, {'$set': data}, upsert)
