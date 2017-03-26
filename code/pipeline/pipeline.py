19188805da016bf73c3adaab9f6df046f7e17905
Bill Best <billtholomew@gmail.com>
2017-03-26 18:04:03 -0400

    Code clean-up and renaming

--- code/pipeline/pipeline.py
+++ code/pipeline/pipeline.py
@@ -6,23 +6,20 @@ from datetime import datetime
 from db.knowhere_db import Reader
 
 
-def iphone(file_with_path, username, dir_to_aggregate=None, processed_dir=None):       
-    df = read_from_csvs(username, file_with_path, dir_to_aggregate, processed_dir)
+def iphone(username, file_in, file_out, commute=False):
+    df = read_from_csvs(username, file_in, file_out)
     df = clean_iphone_data(df)
-    df = aggregate_data(df)
-    return df
-
-    
-def andoid(file_with_path, username, dir_to_aggregate=None, processed_dir=None):
-    raise Exception('Android processing not yet implemented')
-    df = pd.DataFrame()
-    #df = read_from_csvs(username, file_with_path, dir_to_aggregate)
-    #df = clean_android_data(df)
-    #df = aggregate_data(df)
-    #df['user_id'] = user_id
-    return df
+    dd = aggregate_data(df, commute)
+    return dd
     
 
+def android(username, file_in, file_out, commute=False):    
+    df = read_from_csvs(username, file_in, file_out)
+    df = clean_android_data(df)
+    dd = aggregate_data(df, commute)
+    return dd
+    
+    
 def get_user_id(df, username):
     reader = Reader('knowhere')
     user_id = reader.get_user_id(username)
@@ -32,10 +29,16 @@ def get_user_id(df, username):
     return df
 
     
-def read_from_csvs(username, file_with_path, dir_to_aggregate=None, processed_dir=None):
-    if dir_to_aggregate is not None:
-        aggregate_csvs(username, dir_to_aggregate, file_with_path, processed_dir)
-    df = read_single_csv(file_with_path)
+def read_from_csvs(username, file_in, file_out):
+    with open(file_in, 'r+') as fin, open(file_out, 'w+') as fout:
+        fout.write('timestamp, sensor, data_name, data_display, data_raw\n')
+        next(fin)
+        for line in fin:
+            if line[0] is not '"':
+                continue
+            if not search('Screen', line):
+                fout.write(line)
+    df = read_single_csv(file_out)
     df = get_user_id(df, username)
     return df
   
@@ -44,28 +47,13 @@ def read_single_csv(file_with_path):
     df = pd.read_csv(file_with_path, sep=', ', index_col=0, skiprows=0, engine='python', thousands=',', skip_blank_lines=True)
     df.index = pd.to_datetime(df.index.str.replace('"',''))
     return df
-    
-    
-def aggregate_csvs(username, csv_dir, file_with_path, processed_dir):
-    from glob import glob
-    with open(file_with_path, 'w+') as f_handler:
-        f_handler.write('timestamp, sensor, data_name, data_display, data_raw\n')
-        for fn in glob(csv_dir + r'\*{}*'.format(username)):
-            with open(fn, 'r+') as f:
-                next(f)
-                for line in f:
-                    if line[0] is not '"':
-                        continue
-                    if not search('Screen', line):
-                        f_handler.write(line)
-            shutil.move(fn,processed_dir)
-    
-    
+
+        
 def clean_iphone_data(df):
     df = df.applymap(lambda x: str.strip(x) if type(x)==str else x)
     #df = df.applymap(lambda x: pd.to_numeric(x, errors='ignore'))
 	# drop some data we don't need or want
-    desired_sensors = ['GPS', 'Acceleration (via User)', 'Acceleration (via Gravity)', 'Gyrometer (smooth)', 'Magnetometer (raw)', 'Altimeter (Barometer)', 'Microphone']
+    desired_sensors = ['GPS', 'Acceleration (via User)', 'Acceleration (via Gravity)', 'Gyrometer (raw)', 'Magnetometer (raw)', 'Altimeter (Barometer)', 'Microphone']
     df = df[df.data_name != 'Enabled']
     df = df[df.data_name != 'Authorisation Status']
     df = df[df.data_name != 'Floor']
@@ -73,14 +61,40 @@ def clean_iphone_data(df):
     # Rename some sensor data so it's easier to deal with
     df.replace(to_replace={'sensor': {'Acceleration (via Gravity)': 'Gravity', 
                                       'Acceleration (via User)': 'Acceleration',
-									  'Gyrometer (smooth)': 'Gyrometer',
+									  'Gyrometer (raw)': 'Gyrometer',
 									  'Magnetometer (raw)': 'Magnetometer'
 									  }
 						}, inplace=True)
     return df
 
+
+def clean_android_data(df):
+    df = df.applymap(lambda x: str.strip(x) if type(x)==str else x)
+    #df = df.applymap(lambda x: pd.to_numeric(x, errors='ignore'))
+	# drop some data we don't need or want
+    desired_sensors = ['GPS', 'Acceleration', 'Gravity', 'Gyromete', 'Magnetometer', 'Altimeter (Barometer)']
+    df = df[df.data_name != 'Enabled']
+    df = df[df.data_name != 'Authorisation Status']
+    df = df[df.data_name != 'Floor']
+    df = df[df.sensor.isin(desired_sensors)]
+    return df
+
     
-def aggregate_data(df):
+def rename_keys(d, sensor=''):
+    new_dict = {}
+    for key, value in d.items():
+        if isinstance(value, dict):
+            new_value = {}
+            for subkey, subvalue in value.items():
+                new_key = sensor + ' ' + subkey
+                new_value[new_key] = subvalue
+            value = new_value
+        new_dict[key] = value
+
+    return new_dict 
+    
+    
+def aggregate_data(df, commute=False):
     df = df.filter(items=['user_id', 'sensor', 'data_name', 'data_raw'])
     # group by some columns to join the others
     df = df.groupby([df.index, 'user_id', 'sensor']).agg(lambda x: tuple(x))
@@ -91,5 +105,8 @@ def aggregate_data(df):
     df.timestamp = pd.to_datetime(df.timestamp)
     df = df.filter(items=['timestamp', 'user_id', 'sensor', 'data'])	
     df.user_id = df.user_id.apply(ObjectId)
-    return df
+    df['commute'] = str(commute)
+    dd = df.to_dict(orient='records')
+    dd = map(lambda d: rename_keys(d, d['sensor']), dd)
+    return dd
     
